a342570115f5b052f2e4b10e039b4c116858b6a5
eli.x.shi <eli.x.shi@cn.pwc.com>
2024-12-24 13:58:51 +0800

    feat: ✨ add unocss config

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,46 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogSec} className="logo react" alt="React logo" />
-        </a>
-      </div>
+        <div className="text-center">
+          <header className="bg-#282c34 min-w-100vw min-h-100vh flex flex-col items-center justify-center color-white">
+            <a href="https://vite.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogSec} className="logo react" alt="React logo" />
+            </a>
+            <h1 className="mt-2em animate-bounce-alt animate-duration-2s">
+              Hello Vite + React!
+            </h1>
+            <p>
+              <button
+                className="bg-blue-400 hover:bg-blue-500 text-sm text-white font-mono font-light py-2 px-4 rounded border-2 border-blue-200 dark:bg-blue-500 dark:hover:bg-blue-600"
+                type="button"
+                onClick={() => setCount((count) => count + 1)}
+              >
+                count is: {count}
+              </button>
 
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+              <button
+                bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+                text="sm white"
+                font="mono light"
+                p="y-2 x-4"
+                m="l-1em"
+                border="2 rounded blue-200"
+                type="button"
+                onClick={() => setCount((count) => count + 1)}
+              >
+                count is: {count}
+              </button>
+            </p>
+            <p>
+              Edit <code>App.tsx</code> and save to test HMR updates.
+            </p>
+          </header>
+        </div>
+        <div className="h-8 w-8 text-green-600">heelo</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import UnoCSS from 'unocss/vite'
+import presetAttributify from '@unocss/preset-attributify'
+import presetIcons from '@unocss/preset-icons'
+import presetUno from '@unocss/preset-uno'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    UnoCSS({
+      shortcuts: [
+        {
+          logo: 'i-logos-react w-6em h-6em transform transition-800 hover:rotate-180',
+        },
+      ],
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          extraProperties: {
+            'display': 'inline-block',
+            'vertical-align': 'middle',
+          },
+        }),
+      ],
+    }),
+  ],
 })
