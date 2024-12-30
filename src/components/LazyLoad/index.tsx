63d964f08cf92c148f4b32b20e4dbe9b9ca4663b
Eli <elixy@qq.com>
2024-12-30 21:57:39 +0800

    fix: 🐛 modify lazy load func

--- src/components/LazyLoad/index.tsx
+++ src/components/LazyLoad/index.tsx
@@ -1,8 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { lazy, Suspense } from 'react'
 
+const modules = import.meta.glob('@/views/**/index.tsx')
+const components = Object.keys(modules).reduce(
+  (pre: Record<string, () => Promise<any>>, cur) => {
+    const url = cur.replace('/src/views', '').replace('/index.tsx', '')
+    pre[url] = modules[cur]
+    return pre
+  },
+  {},
+)
 // 动态加载组件
-export function LazyLoad(url: string) {
-  const Component = lazy(() => import(`/src/views${url}`))
+type UrlType = keyof typeof components
+export function LazyLoad(url: UrlType) {
+  if (!url) return null
+  const Component = lazy(components[url])
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
